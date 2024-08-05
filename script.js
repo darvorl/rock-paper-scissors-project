5d380fd6026a9be1479e8caf3767105c58db89bf
Dario <valentinorodriguezlozada@gmail.com>
2024-08-05 16:46:38 -0500

    Solved

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log("Hola!")
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let choice = Math.random()
+    if (choice < 0.3) {
+        choice = "scissors"
+    } else if (choice < 0.6) {
+        choice = "paper"
+    } else {
+        choice = "rock"
+    }
+    return choice
+}
+
+function getHumanChoice() {
+    let choice = prompt("Choose rock, paper or scissors! ")
+    return choice
+}
+
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice == "paper" && computerChoice == "scissors") || 
+    (humanChoice == "scissors" && computerChoice == "rock") || 
+    (humanChoice == "rock" && computerChoice == "paper")) {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+        computerScore += 1;
+    } else if ((computerChoice == "paper" && humanChoice == "scissors") || 
+    (computerChoice == "scissors" && humanChoice == "rock") || 
+    (computerChoice == "rock" && humanChoice == "paper")) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        humanScore += 1;
+    } else {
+        console.log(`It's a draw!`)
+    }
+}
+
+function playGame() {
+   playRound(getHumanChoice().toLowerCase(), getComputerChoice());
+   playRound(getHumanChoice().toLowerCase(), getComputerChoice());
+   playRound(getHumanChoice().toLowerCase(), getComputerChoice());
+   playRound(getHumanChoice().toLowerCase(), getComputerChoice());
+   playRound(getHumanChoice().toLowerCase(), getComputerChoice());
+   console.log(`Human score: ${humanScore}`)
+   console.log(`Computer score: ${computerScore}`)
+}
+
+playGame();
